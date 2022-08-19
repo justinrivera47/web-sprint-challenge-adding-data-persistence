b4dc8087fe0eede9cf644b4c676ae6c26822903a
justinrivera47 <justinrivera47@gmail.com>
2022-08-19 11:12:51 -0400

    set up server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
-// build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet())
+server.use(express.json());
+
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  })
+});
+
+module.exports = server;

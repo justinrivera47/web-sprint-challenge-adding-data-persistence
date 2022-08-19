1ba2392ea9206aef67bcaa66eaf248f7e524c36f
justinrivera47 <justinrivera47@gmail.com>
2022-08-19 12:03:55 -0400

    successful connected project router to server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const projectRouter = require('./project/router');
 
 const server = express();
 
 server.use(helmet())
 server.use(express.json());
+server.use('/api', projectRouter)
 
 server.use((err, req, res, next) => {
   res.status(500).json({

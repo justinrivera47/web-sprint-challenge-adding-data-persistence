60938956d921f5466babd6aac80190dd6bbd918d
justinrivera47 <justinrivera47@gmail.com>
2022-08-21 11:40:47 -0400

    I created multiple files for migrations and later realized that did not need to do that so I deleted them off of VS code. I found that I was s ppose to rollback the migration before doing that so I learned my lesson and th t is to delete using rm data/migration/[files]. The tables migrated successfully. I will be building end points to test them out.

--- data/migrations/20220819165228_projects.js
+++ data/migrations/20220819165228_projects.js
@@ -1,15 +1,52 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('projects', tbl => {
+    tbl.increments('project_id')
+      .unsigned();
+    tbl.varchar('project_name', 100)
+      .notNullable();
+    tbl.varchar('project_description', 200)
+    tbl.integer('project_completed')
+  })
+  .createTable('resources', tbl => {
+    tbl.increments('resources_id')
+    .unsigned()
+    tbl.varchar('resource_name', 100)
+    .notNullable();
+    tbl.varchar('resources_description', 600);
+  })
+  .createTable('tasks', tbl => {
+    tbl.increments('task_id')
+      .unsigned()
+    tbl.varchar('task_description', 500)
+      .notNullable()
+    tbl.varchar('task_notes', 500);
+    tbl.integer('task_completed');
+    tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('project_id')
+      .inTable('projects')
+    // tbl.primary(['task_id'], ['project_id'])
+  })
+  .createTable('project_resources', tbl => {
+    tbl.integer('resources_id')
+      .unsigned()
+      .notNullable()
+      .references('resources_id')
+      .inTable('resources')
+    tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('project_id')
+      .inTable('projects')
+    tbl.primary(['resources_id', 'project_id'])
+  })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks');
 };

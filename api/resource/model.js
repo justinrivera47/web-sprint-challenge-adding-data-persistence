4fe4657730036b59c2275f2981ddffc500e14dd7
justinrivera47 <justinrivera47@gmail.com>
2022-08-29 16:48:45 -0400

    can create a resource now

--- api/resource/model.js
+++ api/resource/model.js
@@ -6,4 +6,23 @@ function getAll() {
   return db('resources')
 }
 
-module.exports = {getAll};
+function getById(id) {
+  return db('resources').where('resource_id', id).first()
+}
+
+function findByName(resource_name) {
+  return db('resource_name')
+    .where({ resource_name })
+    .first();
+}
+
+async function createResource(resources) {
+  const [resource_id] = await db('resources').insert(resources)
+  return getById(resource_id)
+}
+
+module.exports = {
+  getAll,
+  getById,
+  createResource
+};
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,11 +1,37 @@
 // build your `/api/resources` router here
 const express = require('express')
-const Project = require('./model')
+const Resource = require('./model')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  const result = await Project.getAll()
-  res.json(result)
+  try {
+    const resources = await Resource.getAll()
+    res.status(200).json(resources)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const resource = await Resource.getById(req.params.id)
+    res.status(200).json(resource)
+  } catch(error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const createdResource = await Resource.createResource(req.body)
+    if(req.body.resource_name === Resource.getAll()){
+      res.status(404).json({ message:'resource already has this name'})
+    } else {
+    res.status(201).json(createdResource)
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 })
 
 module.exports = router

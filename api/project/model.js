29db8f4f0a53e471abe1760743ffcd3c1908aa62
justinrivera47 <justinrivera47@gmail.com>
2022-08-29 15:23:48 -0400

    Got the boolean method to work on the post but I coded it weird. I created a variable project_id and did not refrence it anywhere but it still works.

--- api/project/model.js
+++ api/project/model.js
@@ -2,20 +2,23 @@
 
 const db = require('../../data/dbConfig');
 
+
 function getAll() {
   return db('projects')
     .then(projects => projects.map(p => ({...p, project_completed: p.project_completed ? true : false})))
 }
 
 function getById(id) {
-  return db('projects').where('project_id', id).first();
+  return db('projects').where('project_id', id).first()
 }
 
 async function createProject(project) {
-  const [project_id] = await db('projects').insert(project);
-  return db('projects')
-    .insert(project)
-    
+  const [project_id] = await db('projects').insert(project)
+  const result = {
+    ...project,
+    project_completed: project.project_completed ? true : false,
+  }
+  return result
 }
 
 module.exports = {
